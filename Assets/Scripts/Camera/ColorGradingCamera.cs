306126c2acdf1741e45bbca513f579543040e654
Kishan-Mohan <kishan.mohanakanth@ontariotechu.net>
2025-10-21 12:08:08 -0400

    cameracolorgrading
    camera color grading script

---
+++ Assets/Scripts/Camera/ColorGradingCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ColorGradingCamera : MonoBehaviour
+{
+    public Material lutMaterial; 
+    public Shader colorGradingShader;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Log("OnRenderImage called"); 
+        if(lutMaterial != null)
+        {
+            Graphics.Blit(source, destination, lutMaterial);
+        }
+        else
+        {
+            Graphics.Blit(source, destination); 
+        }
+    }
+}
